ba6a28c097736b2bc379aceb758ab4a192a5a672
Hunter <hgouldthorpe95@gmail.com>
2023-06-04 22:31:59 -0400

    Started site

---
+++ js/main.js
@@ -0,0 +1,33 @@
+const statusButton = document.querySelector("button");
+const pets = document.querySelector(".all-pets");
+
+const createPet = function(name, species) {
+    const pet = {
+        name: name,
+        species: species,
+        isTired: 5,
+        sleep: function () {
+            console.log(`${this.name} needs a nap. Zzz...`);
+            this.isTired = 1;
+        },
+        play: function () {
+            if (this.isTired === 10) {
+                console.log(`${this.name} is too tired to play right now!`);
+                this.sleep();
+            } else {
+                console.log(`Yay! ${this.name} would love to play!`);
+                this.isTired += 1;
+            }
+        }
+    };
+
+    return pet;
+
+};
+
+const sora = createPet("Sora", "ferret");
+const clover = createPet("Clover", "rabbit");
+const baxter = createPet("Baxter", "hamster");
+const cleo = createPet("Cleo", "rat");
+const francine = createPet("Francine", "turtle");
+
